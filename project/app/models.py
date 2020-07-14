1843c8cd4baed15da9ebc5a45b20769fad19bf7f
rajesh kumar <rajesh224kr@gmail.com>
2020-07-14 16:04:35 +0530

    change in models.py

--- project/app/models.py
+++ project/app/models.py
@@ -5,3 +5,4 @@ class Images(models.Model):
     Iname       = models.CharField(max_length=20,null=True,blank=True)
     Ilocation   = models.CharField(max_length=20,null=True,blank=True)
     image       = models.FileField(upload_to='media/')
+    mobile_no       = models.BigIntegerFiled()
